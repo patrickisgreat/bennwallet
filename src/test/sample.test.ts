abe3a6174944fcb6609a4122e027086589f11c57
Patrick Bennett <patrick.bennett@soundcloud.com>
2025-04-30 15:03:10 -0400

    fixing: tests

--- src/test/sample.test.ts
+++
@@ -1,9 +0,0 @@
-import { describe, it, expect } from 'vitest';
-
-// This is a simple test file to ensure tests can run in CI
-
-describe('Basic test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
-  });
-}); 
